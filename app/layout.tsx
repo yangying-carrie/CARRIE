11b649a4248df1ccc456a1799e3d45115cca6254
yangying-carrie <carrie.618.yy@gmail.com>
2025-04-17 17:13:19 +0800

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '../styles/globals.css';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh">
-      <body>{children}</body>
+      <body className="bg-gray-50 text-gray-900 font-sans">{children}</body>
     </html>
   );
 }
